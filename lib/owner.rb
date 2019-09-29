a6fa9fd5c9a42eeb48893e1e3fcfd0c480a5eb70
Erica Basak <ericabasak@gmail.com>
2019-09-29 18:23:44 -0400

    Finished Owner/Dog/Cat modeling.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
-end
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+
+  def clear
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.count
+    Owner.all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+
+end #end of Owner class
